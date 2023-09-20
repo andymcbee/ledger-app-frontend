1b0976640c4c73cb64411a9e0d57f1c31c01ed34
Andrew <armcburn@gmail.com>
2023-09-20 18:38:20 -0400

    Fix backend api url in env

--- src/api/config/config.ts
+++ src/api/config/config.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+
+
 console.log("Test.................................")
 console.log(import.meta.env.VITE_BACKEND_API_ROOT_URL)
+
+const test = process.env.VITE_BACKEND_API_ROOT_URL || import.meta.env.VITE_BACKEND_API_ROOT_URL
+console.log("Or logic for env var:::::::")
+console.log(test)
 
 // initializing the axios instance with custom configs
 export const api = axios.create({
